96f3b413f389a3a2bac60f8a4b0a3cf1d38f1458
Anton Khabibulin <anton8964@gmail.com>
2021-09-07 17:36:41 +0300

    wip

--- index.js
+++ index.js
@@ -1,55 +1,83 @@
+const dedent = require('dedent')
+
 const templateRegex = /template:\s*(?:`)((?:\n|[^`])*)(?:`)(?:}|,)/gm
-
-const generateMapToFile = (text) => ({
-  input: text,
-  chars: text.length,
-  lines: text.split('\n').length,
-  blocks: [],
-  ranges: []
-})
-
-const mapBlockLineToFileLine = (blockLine, textLine, text) => {
-  return text.substring(0, textLine).split('\n').length + blockLine - 1
-}
+const isBlockRegex = /\/(\d+)_\//
+const newLineRegex = /\r\n|\r|\n/
 
 const map = {}
+const mapBuilder = (text) => ({
+  input: text,
+  symbols: text.length,
+  lines: text.split(newLineRegex).length,
+  blocks: [],
+})
+
+const textToLines = (text, substringFrom = 0, substringTo) => {
+  const substring = substringTo !== null
+    ? text.substring(substringFrom, substringTo)
+    : text.substring(substringFrom)
+
+  return substring.split(newLineRegex)
+}
+
+const adjustLine = (baseLine, offsetLine) => {
+  return baseLine + offsetLine - 1;
+}
+
+const adjustColumn = (column) => {
+  return +column > 1 ? column - 1 : column
+}
+
 module.exports = {
   processors: {
     "template-processor": {
       preprocess(text, filename) {
-        if (!map[filename]) {
-          map[filename] = generateMapToFile(text)
-        }
+        map[filename] = map[filename] || mapBuilder(text)
 
-        while(true) {
-          let result = templateRegex.exec(text)
+        let blockIndex = 1
+        while (true) {
+          result = templateRegex.exec(text)
           if (result === null) {
             break
           }
-          const block = { text: result[1], filename }
-          const range = [
-            result.index,
-            result.index + block.text.length
-          ]
-          map[filename].ranges.push(range)
-          map[filename].blocks.push(block)
+
+          const blockText = dedent(result[1])
+          map[filename].blocks.push({
+            input: text,
+            blockIndex: blockIndex++,
+            text: blockText,
+            ranges: [
+              result.index,
+              result.index + blockText.length
+            ],
+            filename
+          })
         }
-        return map[filename].blocks;
+
+        return [{ text, filename }, ...map[filename].blocks]
       },
       postprocess(messages, filename) {
-        for (let blockIdx = 0; blockIdx != messages.length; blockIdx++) {
-          for (let messageIdx = 0; messageIdx != messages[blockIdx].length; messageIdx++) {
-            messages[blockIdx][messageIdx].column = messages[blockIdx][messageIdx].column > 1
-              ? messages[blockIdx][messageIdx].column - 1
-              : 1
-            messages[blockIdx][messageIdx].line = mapBlockLineToFileLine(
-              messages[blockIdx][messageIdx].line,
-              map[filename].ranges[blockIdx][0],
-              map[filename].input,
-            )
+        const isBlock = isBlockRegex.test(filename)
+
+        const basename = isBlock
+          ? filename.split(isBlockRegex)[0]
+          : filename
+
+        const messagesList = [].concat(...messages)
+        for (const message of messagesList) {
+          if (!isBlock) {
+            continue
           }
+          const block = map[basename].blocks[filename.match(isBlockRegex)[1] - 1]
+
+          const [rangeStart] = block.ranges
+          const linesBeforeBlock = textToLines(block.input, 0, rangeStart)
+
+          message.line = adjustLine(linesBeforeBlock.length, message.line)
+          message.column = adjustColumn(message.column)
         }
-        return [].concat(...messages)
+
+        return messagesList
       },
     },
   },
